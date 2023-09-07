d84c0389e459077ee12f88a5d4cbe0d403de63c7
Thang Pham <catrius@linecorp.com>
2023-09-07 23:20:08 +0700

    Footer + Sass

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="py-5 border-top mt-5">
+      <div className="container">
+        <div className="row justify-content-center">
+          <div className="col-lg-7 text-center">
+            <p className="text-muted">
+              This website is made with ❤️ by Catrius
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Header from '@/components/Header';
 import { Outlet } from 'react-router-dom';
+import Footer from '@/components/Footer';
 
 function Layout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <div className="min-vh-100">
+        <Outlet />
+      </div>
+      <Footer />
     </>
   );
 }
