a1c1d15cce16d56f496941cb2fc5ca2c3fd1bac8
Thang Pham <catrius@linecorp.com>
2023-09-11 02:55:02 +0700

    Production

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 export const getPostsAPI = async (page) => axiosInstance.get('posts/', { params: { page } });
