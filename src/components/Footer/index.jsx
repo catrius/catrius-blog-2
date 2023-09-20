77a50561b0aa48838a6885df548da13b3281d176
Thang Pham <catrius@linecorp.com>
2023-09-21 00:45:20 +0700

    Footer - Social links

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Col, Container, Row } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF, faGithub, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import './style.scss';
 
 function Footer() {
   return (
@@ -7,9 +10,40 @@ function Footer() {
       <Container>
         <Row className="justify-content-center">
           <Col lg={7} className="text-center">
-            <p className="text-muted">
-              This website is made with ❤️ by Catrius
-            </p>
+            <div className="mb-4">
+              <a
+                href="https://www.facebook.com/catriuspham/"
+                target="_blank"
+                rel="noreferrer"
+                className="social-link text-decoration-none me-1"
+                aria-label="Facebook"
+              >
+                <FontAwesomeIcon icon={faFacebookF} />
+              </a>
+              <a
+                href="https://github.com/catrius"
+                target="_blank"
+                rel="noreferrer"
+                className="social-link text-decoration-none me-1"
+                aria-label="Github"
+              >
+                <FontAwesomeIcon icon={faGithub} />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/catrius-pham/"
+                target="_blank"
+                rel="noreferrer"
+                className="social-link text-decoration-none me-1"
+                aria-label="Linkedin"
+              >
+                <FontAwesomeIcon icon={faLinkedinIn} />
+              </a>
+            </div>
+            <div className="mb-4">
+              <p className="text-muted">
+                This website is made with ❤️ by Catrius
+              </p>
+            </div>
           </Col>
         </Row>
       </Container>
